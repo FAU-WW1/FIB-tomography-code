bdffd7bebe386d8a16931efdb322c7a8b1a3699e
Tina <martina.heller@fau.de>
2018-03-07 16:43:23 +0100

    smaller version with GUI to select the desired images
    Addition of the GUI to select the desired images for the reconstruction. That's why there is nor more need for the if loop and the variable f. The Output are now two different types of array one cell array and one struct array. (I don't know which one will be more useful)

--- renamezvalue.m
+++ renamezvalue.m
@@ -1,20 +1,27 @@
-function [ thickness ] = renamezvalue( folder )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+function [ thicknesscell, thickstruct] = renamezvalue()
+%This function opens a GUI. The user has to select the images for the
+%reconstruction. The function renames the images with the slicenumber of
+%each image and stores the image again in the same folder. 
+%The z value inside the name is stored in a struct array and as a cell
+%array. Additionally the function calculates the real thickness of each
+%slice.
 
 % files gets all the files inside the folder
-files = dir (folder);
 
+[filename, pathname, ~] = uigetfile( ...
+    {  '*.tif','*.tif images collected by Atlas'}, ...
+    'Select the images for the image stack, please', ...
+    'MultiSelect', 'on');
+%files = dir(filename);
+n = 'name';
+files = cell2struct(filename, n, 1);
 %Creates an empty cell array 
-thickness = {};
-f = 0; %prevents the cell array with empty cells
 t = 0;  %for calculating the real thickness
 
 for i = 1:length(files)
     %Divides the filename in different parts
     [~, fileN, ext] = fileparts(files(i).name);
-    j = length(files(i).name); % j = Length of the name file
-    if j > 10 % Just rename the file if j > 10
+    
         %Get the slicename
         slicen = files(i).name(7:11);
         %Get the thickness z
@@ -22,33 +29,36 @@ for i = 1:length(files)
         %converts the zthick in a number
         zthick = str2double(zthickstr);
         %Write in thickness the slicenumber 
-        thickness {(i-f),1} = slicen;
+        thickstruct(i).SliceNumber = slicen;
         %Write in thickness the zthick
-        thickness {(i-f),2} = zthick;
+        thickstruct(i).TotalThickness = zthick;
        
         %Calculate the real thickness of each slice
         zreal = zthick - t;
         t = t + zreal; 
-        thickness {(i-f),3} = zreal;
+        thickstruct(i).ThicknessPerSlice = zreal;
         
-    %renamefiles FUNCTION
+        %Create a Cell array with the thickness
+        thicknesscell{i,1} = slicen;
+        thicknesscell{i,2} = zthick;
+        thicknesscell{i,3} = zreal;
+        
+       %renamefiles FUNCTION
         %reconstruct oldpath of the file
-        oldname = fullfile (folder,fileN); %Builds the oldpath of the file
+        oldname = fullfile (pathname,fileN); %Builds the oldpath of the file
         B = {oldname, ext};
         oldpath = strjoin(B,''); % Add extension to the Folder Name
         
         %Construct new name of the file with new path
         newname = fileN(7:11); %searched for the numbers in the name
-        newn = fullfile (folder, newname); %Addition of Folder directory and name (without extension)
+        newn = fullfile (pathname, newname); %Addition of Folder directory and name (without extension)
         C = {newn,ext};
         newpath = strjoin(C,''); % Add extension to the Folder Name
         
         %moves & rename the file
         movefile([oldpath],[newpath]);
     
-    else f = f + 1; %prevents the cell array with empty cells
-    
-    end
+end
 
 end
 
